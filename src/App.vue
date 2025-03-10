ae472e0fb58e4c291af17c77f4f9ece23167760e
Masion-coder <2408766306@qq.com>
2025-03-10 23:12:55 +0800

    vue项目介绍&开发流程

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<!-- 定义JS：控制模板部分的数据和行为 -->
+<script setup>
+import { ref } from 'vue';
+
+// 声明响应式数据
+const message = ref('hello vue3');
+</script>
+
+<!-- 模板部分：控制的是页面的结构（html） -->
+<template>
+    <h1>{{ message }}</h1>
+</template>
+
+<!-- 当前组件的css样式 -->
+<style scoped>
+</style>
